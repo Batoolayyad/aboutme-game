767f3dbd4aff11964eed9ae657a5f5147b953966
Batoolayyad <batoolayyad1996@gmail.com>
2021-03-31 00:46:44 +0300

    add q6 and q7

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 
 # LAB- 02
 ## About me, with a guessing game
-* A website that was made to show the visitor 5 questions about the site owner, with yes/no answers so the visitor can have some informations about the site owner, and it will add a comment about the answers if its correct or not.
-* the js code was basically built by if statement, and for the html basic structure: one introductory pragaraph about the site owner.
+* A website that was made to show the visitor 5 questions about the site owner, with yes/no answers so the visitor can have some informations about the site owner, and it will add a comment about the answers if its correct or not. the js code was basically built by if statement, and for the html basic structure: one introductory pragaraph about the site owner.
+
+* add question to ask how many cities you think I visit, out of Jordan?; takes only numbers; compare it to the correct no.
+this done by using if statements inside a for loop.
+
+* add equestion to ask about the cities that I visit; if the visitor answer one correct break the loop and give the auther correct answers; if not give him 6 attempts. 
+this done by using 2 loops iside each others and an array
+
 
 
 
 ### Author: Batool
+with help from:
+ Samer Allaham, teacher
+ Ayman Naif, friend
+
 
 ### Links and resources 
 * [canvas](https://canvas.instructure.com/courses/2625796/assignments/20846796)
@@ -15,10 +25,15 @@
 
 * [unsplash](https://unsplash.com/)
 
+* [css-tricks](https://css-tricks.com/custom-list-number-styling/)
+
 ### Reflections and Comments 
 * Practicing on (if) statement in js.
 * Practicing on normalizing the user input so that it can be validated, by making the code accepting the small and capital letters in the user answers, which problem that I used to face when I start coding.
 * Reset the auto styling and built the style from the beginning , which is something I prefered especially to get rid of the auto margin and padding that the browser add.
 * Adding .gitignore, .eslintec, MIT license files.
+* practicing on loops 
+* practicing on arrays
+
  
 
--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-'use strict'
+'use strict';
 
 // welcoming alert 
-let useName= prompt("Enter your name, please");
-alert ("Welcome "+ useName);
+let userName= prompt("Enter your name, please");
+alert ("Welcome "+ userName);
 alert("lets play a guessing game, all what you have to do is answering with yes or no ");
 
 
 // 5(y/n) questions about the owner
+let scor=0
+
 
 let favoriteFood =prompt ("Is my favorite food Dawaly?");
 favoriteFood= favoriteFood.toLowerCase();
@@ -14,6 +16,7 @@ favoriteFood= favoriteFood.toLowerCase();
 if(favoriteFood=="no"||favoriteFood=="n"){
     //console.log("Correct");
     alert("Correct");
+    scor++
 }else if (favoriteFood=="yes"||favoriteFood=="y") {
     //console.log ("Sorry, not correct, my favorite food is Maghshe");
     alert('Sorry, not correct, my favorite food is Maghshe');
@@ -29,6 +32,7 @@ favoriteColor= favoriteColor.toLowerCase();
 if(favoriteColor=='yes'||favoriteColor=='y'){
     //console.log("Correct");
     alert('correct');
+    scor++
 }else if (favoriteColor=='no'||favoriteColor=='n'){
     //console.log ("Sorry, not correct");
     alert('Sorry, not correct');
@@ -44,6 +48,7 @@ favoriteSport= favoriteSport.toLowerCase();
 if(favoriteSport=="no"||favoriteColor=="n"){
     //console.log("Correct");
     alert('correct');
+    scor++
 }else if(favoriteSport=='yes'||favoriteColor=='y') {
     //console.log ("Sorry not correct, I actually hate running");
     alert("Sorry not correct, I actually hate running");
@@ -59,6 +64,7 @@ favoriteHobby= favoriteHobby.toLowerCase();
 if(favoriteHobby=='yes'||favoriteHobby=='y'){
    //console.log("correct");
     alert('Correct');
+    scor++
 }else if(favoriteHobby=='no'||favoriteHobby=='n') {
    //console.log ("sorry not correct");
     alert("Sorry, not correct");
@@ -74,6 +80,7 @@ favoritePlace= favoritePlace.toLowerCase();
 if(favoritePlace=="yes"||favoritePlace=="y"){
     //console.log("correct");
     alert('correct');
+    scor++
 }else if(favoritePlace=='no'||favoritePlace=='n') {
     //console.log ("sorry not correct");
     alert('Sorry, not correct');
@@ -83,6 +90,77 @@ if(favoritePlace=="yes"||favoritePlace=="y"){
 }
 
 
-// alert thanking the visitor for visiting the site 
-alert(useName+ " thank you for visiting our website");
 
+//q6:how many cities you think I visit, out of Jordan?; takes only numbers; compare it to the correct no.
+
+
+
+for (let i=0; i<5; i++ ){
+
+let citiesNumber= prompt ('How many cities you think I visit out of Jordan?')
+
+let answer= 6;
+
+if (citiesNumber==answer){
+ alert("correct")
+scor++
+break;
+}else if (citiesNumber<=2){
+alert ("too low, try agian" );  
+}
+ else if ( citiesNumber>= 10 ){
+alert ("too big, try agian" );
+}
+else if (3< citiesNumber <9){
+alert("you are close, try agin");
+} 
+else {
+    alert ("enter number please" );}
+
+}
+
+
+
+//q7: ask abuot the cities that I visit; if the visitor answer one correct break the loop and give the auther correct answers; ig not give him 6 attempts 
+    
+let cities=["ny","chicago", "philadelphia", "boston", "dc", "wisconsin dells"];
+let correctAnswer=0;
+
+let userRight=true;
+
+let cityGuesses= prompt('What are the cities that you think I visited out of Jordan?');
+cityGuesses=cityGuesses.toLowerCase();
+
+while(correctAnswer < 6){
+    for (let i=0; i< cities.length; i++){
+    if(cityGuesses=== cities[i]){
+        alert('great, how did you guess!');
+        correctAnswer=6;
+        score=score+1;
+        userRight=false;
+        break;
+    }
+    }
+    correctAnswer++;
+    if( correctAnswer < 6 && userRight===true) {
+    cityGuesses= prompt('I wish if I did visit this city, but I never have been there \n please try again :');
+    }
+    if(correctAnswer >= 6 && userRight===true){
+    alert('You lost');
+    }
+
+}
+
+alert ("these are the cities I visited before: \n ny, chicago, philadelphia, boston, dc, wisconsin dells")
+
+// giving the user his scor 
+alert('well done '+userName+' \n your score is '+scor+'/7'); 
+
+
+// alert thanking the visitor for visiting the site 
+alert(userName+ " thank you for visiting our website");
+
+
+    
+    
+    
